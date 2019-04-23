4d86ba1e8a133540f341314197b20ae3c9d6b7d1
Carlos Andres Hernandez <carlos.andres.hernadez@gmail.com>
2019-04-22 21:26:42 -0500

    Authentication filter

---
+++ src/main/java/com/example/mobileappws/com/example/mobileappws/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.mobileappws.com.example.mobileappws.security;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users";
+    public static final String TOKEN_SECRET = "iccid:4454788545885968585##nonce:5e452610c9f9decd:-404fb717:157fd6fc54e:-7ffe##timestamp:2016-10-25T16:16:36.036Z";
+}
---
+++ src/main/java/com/example/mobileappws/com/example/mobileappws/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.mobileappws.com.example.mobileappws.service;
+
+import com.example.mobileappws.com.example.mobileappws.shared.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService extends UserDetailsService {
+    UserDto createUser(UserDto user);
+}
